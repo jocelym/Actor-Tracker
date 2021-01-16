4764781ff7f3b77ed81861945f0ada0f200b100b
Eugeneh1217 <Eugeneh1217@gmail.com>
2021-01-16 13:50:03 -0500

    changed Compare.py so compare() takes image urls instead of jpg references

--- BackEnd/Compare.py
+++ BackEnd/Compare.py
@@ -1,6 +1,8 @@
 from face_recognition import compare_faces
 from face_recognition import face_encodings
 from face_recognition import load_image_file
+from urllib.request import urlretrieve
+import os
 
 """
 Module to compare actors in frame with actors in dictionary
@@ -14,9 +16,13 @@ def compare(actor, frame):
     actor: reference to jpg with just actor's face
     frame: reference to jpg of frame from movie
     """
-    frame_encoding = face_encodings(load_image_file(frame))
+    urlretrieve(actor, 'actor.jpg')
+    urlretrieve(frame, 'frame.jpg')
+    frame_encoding = face_encodings(load_image_file('frame.jpg'))
+    actor_encoding = face_encodings(load_image_file('actor.jpg'))[0]
+    os.remove('actor.jpg')
+    os.remove('frame.jpg')
     for encoding in frame_encoding:
-        if compare_faces([face_encodings(load_image_file(actor))[0]], encoding):
+        if compare_faces([actor_encoding], encoding):
             return True
     return False
-
