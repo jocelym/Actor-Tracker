3323be66e342504f41603017860461b7a2bae3d9
Brielle-Chenier <brielle_chenier@live.com>
2021-01-16 11:37:38 -0500

    basic ui

--- chromeExtension/popup/popup.js
+++ chromeExtension/popup/popup.js
@@ -1,17 +1,19 @@
 let search_actor = document.getElementById('searchActor');
-
-chrome.storage.sync.get('color', function(data) {
+let actor_name = document.getElementById('actorName');
+let actor_other_movies = document.getElementById('actorOtherMovies');
+/*chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
-});
+}); */
 
-search_actor.onclick = function(element) {
+search_actor.onclick = function() {
+  var foundName = "Famous Person";
+  var foundOtherMovies = "Cool Movies"
+  document.getElementById("actorName").innerHTML = (
+    "Actor Name: " + foundName);
+  document.getElementById("actorOtherMovies").innerHTML = (
+    "Also in: " + foundOtherMovies);
   alert("buttonPressed!")
 
-/*let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });*/
+
 };
