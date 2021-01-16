95ca821176b578dec2e855a688950449b1874dc1
Brielle-Chenier <brielle_chenier@live.com>
2021-01-16 11:07:12 -0500

    small changes

--- chromeExtension/popup/popup.js
+++ chromeExtension/popup/popup.js
@@ -1,15 +1,17 @@
-let changeColor = document.getElementById('changeColor');
+let search_actor = document.getElementById('searchActor');
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
+search_actor.onclick = function(element) {
+  alert("buttonPressed!")
+
+/*let color = element.target.value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+  });*/
 };
