7a1d8a5f9968b70cbebbc4d849f25c2802a8bfec
Jocelyn Lim <jocelynlim@v1041-wn-rt-d-162-162.campus-dynamic.uwaterloo.ca>
2021-01-16 11:07:41 -0500

    add button functionality

--- extension/notes.js
+++ extension/notes.js
@@ -10,6 +10,7 @@ function clear_notes() {
 function add_notes(notes) {
   var body = document.getElementsByTagName("body")[0];
   for (let i = 0; i < notes.length; i++) {
+    console.log('pls work');
     noteCount += 1;
     var noteNode = document.createElement("div");
     noteNode.setAttribute("id", "htn-sticky-note-" + noteCount);
@@ -72,6 +73,8 @@ chrome.runtime.onMessage.addListener(
     if (request.action == "clear") {  // Delete All Notes on the Page
       clear_notes();
       sendResponse({status: "complete"});
+      console.log('pls work');
+
     }
     else if (request.action == "add") { // Add New Notes to Page
       add_notes(request.notes);
@@ -82,6 +85,24 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+if (window == top) {
+  window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
+    console.log("Hello");
+  }
+  
+  trigger_key = 71; // g key
+  function doKeyPress(e){
+    if (e.shiftKey && e.keyCode == trigger_key){ // if e.shiftKey is not provided then script will run at all instances of typing "G"
+      console.log('Hi!');
+      console.log("Hello again");
+
+      chrome.tabs.captureVisibleTab((screenshotUrl) => {
+        const string = chrome.extension.getURL('screenshot.html?id=' + id++)
+        let targetId = null;
+      })
+    }
+  }
 
 
 
