9abaea6481ea9b0c595d8772ac9ae03b44a22764
Eugeneh1217 <Eugeneh1217@gmail.com>
2021-01-17 00:36:51 -0500

    updated joined.py with new Compare.py

--- BackEnd/joined.py
+++ BackEnd/joined.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import base64
 from bs4 import BeautifulSoup
 import calendar
 from face_recognition import compare_faces
@@ -9,24 +10,23 @@ from urllib.request import urlretrieve
 import os
 
 
-"""
-Module to compare actors in frame with actors in dictionary
-"""
-
-
 def compare(actor, frame):
     """
     determine whether actor is in frame
 
-    actor: reference to jpg with just actor's face
-    frame: reference to jpg of frame from movie
+    actor: url to jpg
+    frame: 64bit jpg encoding string (ignoring first 22 characters)
     """
     urlretrieve(actor, 'actor.jpg')
-    urlretrieve(frame, 'frame.jpg')
-    frame_encoding = face_encodings(load_image_file('frame.jpg'))
+
+    with open("frame_image.jpg", 'wb') as frame_image:
+        frame_image.write(base64.b64decode(frame[23:]))
+
     actor_encoding = face_encodings(load_image_file('actor.jpg'))[0]
+    frame_encoding = face_encodings(load_image_file('frame_image.jpg'))
+
     os.remove('actor.jpg')
-    os.remove('frame.jpg')
+    os.remove('frame_image.jpg')
     for encoding in frame_encoding:
         if compare_faces([actor_encoding], encoding):
             return True
