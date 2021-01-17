94ad1b0ed8602f8e4651ac2d6e3a47f92ebd6057
IsabellaSchneider7 <71522975+IsabellaSchneider7@users.noreply.github.com>
2021-01-16 20:04:59 -0500

    back end done?

--- BackEnd/Main.py
+++ BackEnd/Main.py
@@ -1,11 +1,11 @@
+import json
+
+
 def main(moviename, frame):
     actors = getactors(moviename)
     actorsinfo = []
     for actor in actors:
-        """ need to send just actorimage? or whole actor: (img,link)... need to break up actor into groups"""
-        actorlink = actors[actor]
-        actorpic = actors[actor["Image"]]
-        if compare(actorpic, frame):
-            actorsinfo[actor] = getactorinfo(actors[actor])
-
-    return actorsinfo
+        if compare(actor["Image"], frame):
+            actorsinfo.append(getactorinfo(actor))
+    finalinfo = {"Data": actorsinfo}
+    return json.dumps(finalinfo)
--- BackEnd/ScrapeInfo.py
+++ BackEnd/ScrapeInfo.py
@@ -1,13 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import calendar
+
 
 def getactorinfo(actor):
-    webimbd = requests.get(actor[Link])
+    webimbd = requests.get(actor["Link"])
     web = BeautifulSoup(webimbd.text, "html.parser")
     tbody = web.find('time')
     born = tbody['datetime'].split("-")
     born[1] = calendar.month_name[int(born[1])]
+    actor["Born"] = born[1] + " " + born[2] + ", " + born[0]
     top3 = []
     for film in web.find_all('div', {'class': 'knownfor-title-role'}):
         film = film.find('a')
         top3.append(film['title'])
-    information = {"Born": born[1] + " " + born[2] + ", " + born[0], "Known For": top3}
-    return information
+    actor["Known For"] = top3
+    return actor
